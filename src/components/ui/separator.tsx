f6d1aaacfe01b18f99bf780a1d0f120badc485bb
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-03-12 20:53:17 +0330

    🔧 separator component not rendering

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -15,8 +15,13 @@ function Separator({
       data-slot="separator-root"
       decorative={decorative}
       orientation={orientation}
+      // className={cn(
+      //   'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
+      //   className,
+      // )}
       className={cn(
-        'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
+        'bg-border shrink-0',
+        orientation === 'horizontal' ? 'h-[1px] w-full' : 'min-h-full w-[1px]',
         className,
       )}
       {...props}
